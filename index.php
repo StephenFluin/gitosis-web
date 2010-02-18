cda15f0491662226a9d2b01b8b941031d148adac
Stephen Fluin <stephen@mistuph.com>
2010-02-17 19:47:10 -0600

    Working on interface for connecting to gitosis-admin.

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 
 require("functions.inc.php");
 
+
+if($_POST["path"]) {
+	$_SESSION["gitosisurl"] = $_POST["path"];
+}
+
+
 if($_SESSION["gitosisurl"]) {
 	// Iniital version will require a locally checked out version of gitosis-admin belonging to www-data. This means the user will have to setup gitosis, add a key for www-data before being able to use this web tool.
+	$dir = $_SESSION["gitosisurl"];
+	if(file_exists($dir)) {
+		$body .= "Found the path.  ";
+	} else {
+		$body .= "Didn't find the path.  ";
+	}
 
+	if(file_exists($dir . ".git/config") && file_exists($dir . "gitosis.conf")) {
+		$body .= "Valid gitosis-admin repo found.  ";
+	}
+	$body .= '<a href="?action=logout">Log Out</a>';
 
 } else {
 	$body .= "Please enter the path on the server for the checked out version of gitosis-admin:";
-	$body .= "<form method="post"><input type="text" name="path"/><button type="submit">Setup</button>	</form>";
+	$body .= '<form method="post"><input type="text" name="path"/><button type="submit">Setup</button>	</form>';
 }
 
 print $body;
